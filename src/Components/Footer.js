47125228aab7c59409e57757fae9ec1df95dbed3
John-Zoscak-02 <jzoscak02@gmail.com>
2022-08-09 19:09:46 -0400

    update ln icon

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,8 @@ import {Fade} from 'react-reveal';
 
 //import Github from '../images/icons/github.png';
 import Github from '../images/icons/github-dark.png';
-import Linkedin from '../images/icons/linkedin.png';
+//import Linkedin from '../images/icons/linkedin.png';
+import Linkedin from '../images/icons/linkedin-dark.png';
 import JZ from '../images/jz.png';
 
 const Footer = (props) => {
--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -4,7 +4,8 @@ import { Fade } from 'react-reveal';
 import { IconButton } from '@mui/material';
 //import Github from '../images/icons/github.png';
 import Github from '../images/icons/github-dark.png';
-import Linkedin from '../images/icons/linkedin.png';
+//import Linkedin from '../images/icons/linkedin.png';
+import Linkedin from '../images/icons/linkedin-dark.png';
 
 const Icons = (props) => {
   return (
