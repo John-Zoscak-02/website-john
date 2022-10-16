7611e0e1e9e985a6d959e24821281d2311432e07
John-Zoscak-02 <jzoscak02@gmail.com>
2022-10-16 10:05:35 -0400

    Resume update

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -9,13 +9,13 @@ const Featured = (props) => {
     <div className='featured'>
       <Fade big>
         <Divider variant="middle" classes={{ root: props.classes.divider }} />
-        <Link href='https://drive.google.com/file/d/18QU2s1NE7oH3m_pkaVTUJbcPM31CBrYl/preview' target='_blank' color='inherit' underline='hover'>
+        <Link href='https://drive.google.com/file/d/1yc_3fkqOCw-X9R7KARSkxEjLozVH1o83/preview' target='_blank' color='inherit' underline='hover'>
           <Typography className='grow' variant='h3'>Resume</Typography>
         </Link>
       </Fade>
       <div className='pdf'>
         <Fade big>
-          <iframe title='John Zoscak Resume, 2021' src='https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=18QU2s1NE7oH3m_pkaVTUJbcPM31CBrYl&hl=en&embedded=true' alt='PDF of Resume' frameborder="0" />
+          <iframe title='John Zoscak Resume, 2021' src='https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1yc_3fkqOCw-X9R7KARSkxEjLozVH1o83&hl=en&embedded=true' alt='PDF of Resume' frameborder="0" />
         </Fade>
       </div>
     </div>
