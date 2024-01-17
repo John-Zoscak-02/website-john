d32d7ec76bb16d30414a5e04e0187998815ce24e
John-Zoscak-02 <jzoscak02@gmail.com>
2024-01-17 15:35:07 -0500

    New resume

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -15,7 +15,8 @@ const Featured = (props) => {
       </Fade>
       <div className='pdf'>
         <Fade big>
-          <iframe title='John Zoscak Resume, 2021' src='https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1P9mAYOgvDkZ37yyKtMP0hiNxkxp2Y-hK&hl=en&embedded=true' alt='PDF of Resume' frameborder="0" />
+          {/*<iframe title='John Zoscak Resume, 2021' src='https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1P9mAYOgvDkZ37yyKtMP0hiNxkxp2Y-hK&hl=en&embedded=true' alt='PDF of Resume' frameborder="0" />*/}
+          <iframe title='John Zoscak Resume, 2021' src='https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1AE9bWNA54deZ1eaMw1Fgjx3tho2LSy4y&hl=en&embedded=true' alt='PDF of Resume' frameborder="0" />``
         </Fade>
       </div>
     </div>
