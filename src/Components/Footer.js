f9bf724bdb1ace499f15411c325492f2327cdaf7
John-Zoscak-02 <jzoscak02@gmail.com>
2025-10-09 17:10:08 -0400

    updated the landing and the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { IconButton, Link } from '@mui/material';
+import { IconButton, Link, Typography} from '@mui/material';
 
 // import Github from '../images/icons/github.png';
 import Github from '../images/icons/github-dark.png';
@@ -18,6 +18,15 @@ const Footer = (props) => {
           <div class="divider left"></div>
           <img className="top-center center" src={Center} alt="floral center"></img>
           <div class="divider right"></div>
+      </div>
+      <div className="grid-center">
+        <Typography type="subtitle1" fontWeight="bold">Rom. 8:38-39</Typography>
+        <Typography type="subtitle1">For I am persuaded that neither death nor life, nor angels nor principalities nor powers, nor things present nor things to come, nor height nor depth, nor any other created thing, shall be able to separate us from the love of God which is in Christ Jesus our Lord.
+          <br></br>
+          <br></br>
+        </Typography>
+        <Typography type="subtitle1" fontWeight="bold">John. 17:25-26</Typography>
+        <Typography type="subtitle1">“Righteous Father, though the world does not know you, I know you, and they know that you have sent me. I have made you known to them, and will continue to make you known in order that the love you have for me may be in them and that I myself may be in them.”</Typography>
       </div>
       <div className='footer-content'>
         <div className='contact'>
